25e343d87da4374e5e5ab20329c56c1826b9e176
aswanth-mc <mcaswanth5@gmail.com>
2026-02-18 21:07:16 +0530

    feat : add tasks are store in local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,22 +3,60 @@ const form = document.getElementById("task-form");
 const input = document.getElementById("task-input");
 const taskList = document.getElementById("task-list");
 
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
 // Add Task
 form.addEventListener("submit", function (e) {
     e.preventDefault(); // prevent page reload
 
     const taskText = input.value.trim();
-
     if (taskText === "") return; // avoid empty tasks
 
-    const li = document.createElement("li");
-    li.innerHTML = `
-        <input type="checkbox">
-        <span>${taskText}</span>
-    `;
+    const task ={
+        id: Date.now(),
+        text: taskText,
+        completed: false
+    };
+    tasks.push(task);
 
-    taskList.appendChild(li);
-
-    // Clear input
+    saveTasks();
+    renderTasks();
     input.value = "";
 });
+
+
+    function saveTasks() {
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+    }
+
+    function renderTasks() {
+    taskList.innerHTML = "";
+
+    tasks.forEach(task => {
+        const li = document.createElement("li");
+
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.checked = task.completed;
+
+        checkbox.addEventListener("change", function () {
+            task.completed = this.checked;
+            saveTasks();
+            renderTasks();
+        });
+
+        const span = document.createElement("span");
+        span.textContent = task.text;
+
+        if (task.completed) {
+            span.style.textDecoration = "line-through";
+        }
+
+        li.appendChild(checkbox);
+        li.appendChild(span);
+
+        taskList.appendChild(li);
+    });
+}
+
+renderTasks();
